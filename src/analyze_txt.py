77ef65b2244b6af68775b5255853f69eb1229d3e
Colin Wallace <wallacoloo@gmail.com>
2016-06-06 13:36:36 -0700

    Use VADER instead of SWN; also use nltk.sent_tokenize to split by sentences instead of regular expressions

--- src/analyze_txt.py
+++ src/analyze_txt.py
@@ -3,49 +3,34 @@
 associations to a json file
 """
 
-import json, re, sys
-from nltk.corpus import sentiwordnet as swn
-# Sentence boundary occurs as punctuation (. ! ?) followed by at least
-# one space, followed by a capital letter. Note that double-qoutes in
-# in this region should be accepted (entering or leaving a quote).
-sentence_boundary = re.compile(r'(?<=[.!?])"?\s+"?(?=[A-Z])', re.M)
-word_boundary = re.compile(r'\s')
-valid_word_chars = "-_abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+import json, sys
+from nltk import tokenize
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
-characters = ["Applejack", "Fluttershy", "Pinkie Pie", "Rainbow Dash", "Rarity", "Twilight Sparkle"]
+characters = ("Applejack", "Fluttershy", "Pinkie Pie", "Rainbow Dash", "Rarity", "Twilight Sparkle")
 
-def rank_sentiment(s):
-    """Return how 'positive' a sentence is.
-    This is done using data from SentiWordNet, which ranks words based
-    on their: positivity, negativity and objectivity.
-    """
-    # nltk SentiWordNet example: http://www.nltk.org/howto/sentiwordnet.html
-    rank = 0
-    for word_raw in re.split(word_boundary, s):
-        word = "".join(c for c in word_raw if c in valid_word_chars)
-        # each word can match multiple (or 0) uses
-        matches = list(swn.senti_synsets(word))
-        if matches: # avoid div-by-0
-            rank += sum(m.pos_score() - m.neg_score() for m in matches)/len(matches)
-    return rank
+sid = SentimentIntensityAnalyzer()
 
 def analyze(f):
     """Return a dict of information about the file f"""
-    sentences = re.split(sentence_boundary, f.read())
+    sentences = tokenize.sent_tokenize(f.read())
     print(sentences)
 
     sentiment = {}
-    for c in characters:
+    for c in characters + ("text",):
         sentiment[c] = {"raw": []}
 
     for s in sentences:
+        compound = sid.polarity_scores(s)["compound"]
+        # Track sentiment of the overall text
+        sentiment["text"]["raw"].append(compound)
+        # Try to assign the sentiment to a SINGLE character
         c_in_s = [c for c in characters if c in s]
         if len(c_in_s) == 1:
-            # If the sentence contains multiple names, it would be very
-            # easy to attribute the opinion to the wrong character.
             c = c_in_s[0]
-            sentiment[c]["raw"].append(rank_sentiment(s.replace(c, "")))
+            sentiment[c]["raw"].append(compound)
 
+    # Compute average sentiments per character and entire text
     for p in sentiment.values():
         p["avg"] = sum(p["raw"])/len(p["raw"]) if p["raw"] else 0
 
